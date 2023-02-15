46c611e9acc618e21ce693b25db4558af6f8c68a
rkga2021 <rkga2021@mymail.pomona.edu>
2023-02-14 23:06:56 -0800

    take 6

--- word_ladder.py
+++ word_ladder.py
@@ -1,54 +1,26 @@
-#!/bin/python3
+#!/bin/python3:
 import copy
 from collections import deque
 
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
-    '''
-    Returns a list satisfying the following properties:
-
-    1. the first element is `start_word`
-    2. the last element is `end_word`
-    3. elements at index i and i+1 are `_adjacent`
-    4. all elements are entries in the `dictionary_file` file
-
-    For example, running the command
-    ```
-    word_ladder('stone','money')
-    ```
-    may give the output
-    ```
-    ['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny',
-    'boney', 'money']
-    ```
-    but the possible outputs are not unique,
-    so you may also get the output
-    ```
-    ['stone', 'shone', 'shote', 'shots', 'soots', 'hoots', 'hooty', 'hooey',
-    'honey', 'money']
-    ```
-    (We cannot use doctests here because the outputs are not unique.)
-
-    Whenever it is impossible to generate a word ladder between the two words,
-    the function returns `None`.
-    '''
-    with open(dictionary_file, 'r') as afile:
-        words_list = [line.strip() for line in afile]
-    if end_word not in words_list:
+    with open(dictionary_file, 'r') as f:
+        words = [line.strip() for line in f]
+    if end_word not in words:
         return None
     if start_word == end_word:
         return [start_word]
     ladder = deque([[start_word]])
     while ladder:
-        route = ladder.popleft()
-        last_word = route[-1]
-        for word in words_list[:]:
-            if word not in route and _adjacent(last_word, word):
+        path = ladder.popleft()
+        last_word = path[-1]
+        for word in words[:]:
+            if word not in path and _adjacent(last_word, word):
                 if word == end_word:
-                    return route + [end_word]
+                    return path + [end_word]
                 else:
-                    ladder.append(copy.deepcopy(route) + [word])
-                    words_list.remove(word)
+                    ladder.append(copy.deepcopy(path) + [word])
+                    words.remove(word)
     return None
 
 
@@ -86,9 +58,10 @@ def _adjacent(word1, word2):
     '''
     if len(word1) != len(word2):
         return False
-    diff_charcounter = 0
+    num_diff = 0
     for i in range(len(word1)):
-        diff_charcounter += 1
-        if diff_charcounter > 1:
-            return False
-    return diff_charcounter == 1
+        if word1[i] != word2[i]:
+            num_diff += 1
+            if num_diff > 1:
+                return False
+    return num_diff == 1
